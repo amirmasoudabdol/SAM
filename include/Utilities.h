3e90e174c9dd16b56c6d7cfdae79aa720379ecc7
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2020-02-12 14:59:06 +0100

    added more serializers

--- include/Utilities.h
+++ include/Utilities.h
@@ -15,12 +15,85 @@
 #include "sam.h"
 #include "nlohmann/json.hpp"
 #include "effolkronium/random.hpp"
+#include "mvrandom.hpp"
 
 using json = nlohmann::json;
 using Generator = std::mt19937;
 using Distribution = std::function<double(Generator &)>;
 using MultivariateDistribution = std::function<arma::mat(Generator &)>;
 using Random = effolkronium::random_static;
+
+Distribution make_distribution(json const &j);
+MultivariateDistribution make_multivariate_distribution(json const &j);
+
+template<typename T = double>
+std::vector<T>
+get_expr_setup_params(json const &j, int const size) {
+    
+    switch (j.type()) {
+        case nlohmann::detail::value_t::array:
+            {
+                if (j.size() != size)
+                    throw std::length_error("Array size does not match with the size\
+                                        of the experiment.\n");
+                else
+                    return j.get<std::vector<T>>();
+            }
+            break;
+            
+        case nlohmann::detail::value_t::number_integer:
+        case nlohmann::detail::value_t::number_unsigned:
+        case nlohmann::detail::value_t::number_float:
+            return std::vector<T>(size, j.get<T>());
+            break;
+            
+        case nlohmann::detail::value_t::null:
+        default:
+            throw std::invalid_argument("Missing parameter.\n");
+            break;
+    }
+    
+}
+
+template <class DistributionType, class... Parameters>
+Distribution make_distribution_impl(json const &j, Parameters... parameters) {
+    return DistributionType{j.at(parameters)...};
+}
+
+template <class DistributionType, class... Parameters>
+MultivariateDistribution make_multivariate_distribution_impl(json const &j, Parameters... parameters) {
+    return DistributionType{j.at(parameters)...};
+}
+
+
+
+/**
+ \brief      A helper function to fill `val` to a vector. This
+ will also allocate the necessary space, `size`, for the vector.
+ 
+ \param      vecc  The reference to the vector
+ \param[in]  size  The size of the vector
+ \param[in]  val   The value of the vector
+ 
+ \tparam     T     The type of the vector and value.
+ 
+ TODO: Just use the return value here, passing by reference is not safe and
+ necessary, also due to the move sematic, I can just do this easily without
+ preformance lose.
+ */
+template<typename T>
+void fill_vector(arma::Row<T> &vecc, int size, T val){
+    vecc = arma::Row<T>(size).fill(val);
+};
+
+arma::Mat<double>
+constructCovMatrix(const arma::Row<double> &stddevs, const arma::Row<double> &covs, int n);
+
+arma::Mat<double>
+constructCovMatrix(const arma::Row<double> &stddevs, const double cov, int n);
+
+arma::Mat<double>
+constructCovMatrix(const double stddev, const double cov, const int n);
 
 namespace arma {
     
@@ -79,132 +152,94 @@ namespace arma {
 
 }
 
-// JSON to ARMA Serializer
 namespace nlohmann {
+
+
     template <typename T>
-    struct adl_serializer<arma::Mat<T>> {
-        static void to_json(json& j, const arma::Mat<T> &mat) {
-            std::vector<std::vector<T>> vmat(mat.n_rows);
-            
-            for (int i = 0; i < mat.n_rows; ++i)
-                vmat[i] = arma::conv_to<std::vector<T>>::from(mat.row(i));
-
-            j = *mat;
-
-        }
+    struct adl_serializer<mvrandom::mvnorm_distribution<T>> {
         
-        static void from_json(const json& j, arma::Mat<T>& mat) {
-            /// TODO: Check it vac is actually a matrix.
-            /// TODO: This is almost working but I need to translate
-            /// everything into `arma::vec` to make this more uniform all the way.
-            int n_rows, n_cols;
-            std::vector<std::vector<T>> vmat;
-            if (j[0].type() != detail::value_t::array) {
-                vmat.push_back(j.get<std::vector<T>>());
-            }else if (j[0].type() == detail::value_t::array) {
-                vmat = j.get<std::vector<std::vector<T>>>();
-            }else{
-                throw std::invalid_argument("Cannot convert the given value to a matrix."); // It's not a matrix
-            }
-            
-            n_rows = vmat.size();
-            n_cols = vmat[0].size();
-            
-            /// TODO: This is dirty but it return the transpose of the matrix, so
-            /// it'll be `arma::vec` instead of `vec`.
-            /// FIXME!
-            mat = arma::Mat<T>(n_cols, n_rows);
-            for (int i = 0; i < n_rows; ++i) {
-                for (int j = 0; j < n_cols; ++j) {
-                    mat(j, i) = vmat[i][j];
-                }
-            }
+        static void to_json(json &j, const mvrandom::mvnorm_distribution<T> &mdist) {
+
+            j["dist"] = "mvnorm_distribution";
+            j["means"] = arma::rowvec(mdist.means().as_row());
+            j["sigma"] = mdist.sigma();
             
         }
+
+        static mvrandom::mvnorm_distribution<T> from_json(const json &j) {
+            
+            auto means = j.at("means").get<arma::Row<double>>();
+            arma::Mat<T> sigma;
+            auto n_dims = means.n_elem;
+            
+            if (j.find("sigma") != j.end()){
+                sigma = j.at("sigma").get<arma::Mat<double>>();
+                
+            }else{
+                if (j.find("stddevs") == j.end() || j.find("covs") == j.end()){
+                    std::invalid_argument("Either `sigma` or `covs` and `stddevs` have to be given.");
+                }
+                arma::rowvec stddevs = get_expr_setup_params(j.at("stddevs"), n_dims);
+                arma::rowvec covs = get_expr_setup_params(j.at("covs"), n_dims * (n_dims - 1) / 2);
+                // construct sigma
+                sigma = constructCovMatrix(stddevs, covs, n_dims);
+            }
+
+            return mvrandom::mvnorm_distribution<T>{means.t(), sigma};
+        }
+
     };
-}
 
-Distribution make_distribution(json const &j);
-MultivariateDistribution make_multivariate_distribution(json const &j);
 
-template <class DistributionType, class... Parameters>
-Distribution make_distribution_impl(json const &j, Parameters... parameters) {
-    return DistributionType{j.at(parameters)...};
-}
+    template <typename T>
+    struct adl_serializer<mvrandom::truncated_mvnorm_distribution<T>> {
+        
+        static void to_json(json &j, const mvrandom::truncated_mvnorm_distribution<T> &mdist) {
 
-template <class DistributionType, class... Parameters>
-MultivariateDistribution make_multivariate_distribution_impl(json const &j, Parameters... parameters) {
-    return DistributionType{j.at(parameters)...};
-}
+            j["dist"] = "truncated_mvnorm_distribution";
+            j["means"] = arma::rowvec(mdist.means().as_row());
+            j["sigma"] = mdist.sigma();
+            j["lowers"] = arma::rowvec(mdist.lowers().as_row());
+            j["uppers"] = arma::rowvec(mdist.uppers().as_row());
+            
+        }
 
-template<typename T = double>
-std::tuple<std::vector<T>, Distribution>
-get_expr_setup_params(json const &j, int const size) {
-    
-    switch (j.type()) {
-        case nlohmann::detail::value_t::object:
-            try {
+        static mvrandom::truncated_mvnorm_distribution<T> from_json(const json &j) {
+            
+            auto means = j.at("means").get<arma::Row<double>>();
+            auto n_dims = means.n_elem;
+            
+            arma::Mat<T> sigma;
+            arma::Mat<T> lowers, uppers;
+            
+            if (j.find("sigma") != j.end()){
+                sigma = j.at("sigma").get<arma::Mat<double>>();
+                if (sigma.n_elem != n_dims * n_dims) {
+                    std::domain_error("`sigma` doesn't have the correct size.");
+                }
+            }else{
+                if (j.find("stddevs") == j.end() || j.find("covs") == j.end())
+                    std::invalid_argument("Either `sigma` or `covs` and `stddevs` have to be given.");
                 
-                auto dist = make_distribution(j);
-                auto val = Random::get(dist);
-                
-                return std::make_tuple(std::vector<T>(size, val), dist);
-                
-            } catch (const std::exception& e) {
-                throw e.what();
+                arma::rowvec stddevs = get_expr_setup_params(j.at("stddevs"), n_dims);
+                arma::rowvec covs = get_expr_setup_params(j.at("covs"), n_dims * (n_dims - 1) / 2);
+                // construct sigma
+                sigma = constructCovMatrix(stddevs, covs, n_dims);
             }
-            break;
             
-        case nlohmann::detail::value_t::array:
-            {
-                if (j.size() != size)
-                    throw std::length_error("Array size does not match with the size\
-                                        of the experiment.\n");
-                else
-                    return std::make_tuple(j.get<std::vector<T>>(), nullptr);
-            }
-            break;
-            
-        case nlohmann::detail::value_t::number_integer:
-        case nlohmann::detail::value_t::number_unsigned:
-        case nlohmann::detail::value_t::number_float:
-            return std::make_tuple(std::vector<T>(size, j.get<T>()), nullptr);
-            break;
-            
-        case nlohmann::detail::value_t::null:
-        default:
-            throw std::invalid_argument("Missing parameter.\n");
-            break;
-    }
-    
+            lowers = get_expr_setup_params(j.at("lowers"), n_dims);
+            uppers = get_expr_setup_params(j.at("uppers"), n_dims);
+
+            return mvrandom::truncated_mvnorm_distribution<T>{means.t(), sigma, lowers, uppers};
+
+        }
+
+    };
+
 }
 
-/**
- \brief      A helper function to fill `val` to a vector. This
- will also allocate the necessary space, `size`, for the vector.
- 
- \param      vecc  The reference to the vector
- \param[in]  size  The size of the vector
- \param[in]  val   The value of the vector
- 
- \tparam     T     The type of the vector and value.
- 
- TODO: Just use the return value here, passing by reference is not safe and
- necessary, also due to the move sematic, I can just do this easily without
- preformance lose.
- */
-template<typename T>
-void fill_vector(arma::Row<T> &vecc, int size, T val){
-    vecc = arma::Row<T>(size).fill(val);
-};
 
-arma::Mat<double>
-constructCovMatrix(const arma::Row<double> &stddevs, const arma::Row<double> &covs, int n);
 
-arma::Mat<double>
-constructCovMatrix(const arma::Row<double> &stddevs, const double cov, int n);
 
-arma::Mat<double>
-constructCovMatrix(const double stddev, const double cov, const int n);
 
 #endif //SAMPP_UTILITIES_H
