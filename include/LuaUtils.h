44d618d17af64fbafa5c0e5d564553edea9c974b
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2020-03-10 10:48:04 +0100

    I think I want to move sol related stuff to LuaUtils but I didn't do it because I had to deal with include problems and I didn't feel it

---
+++ include/LuaUtils.h
@@ -0,0 +1,20 @@
+//
+// Created by Amir Masoud Abdol on 2019-02-01.
+//
+
+#ifndef SAMPP_LUAUTILS_H
+#define SAMPP_LUAUTILS_H
+
+#include <utility>
+
+#include <sol/sol.hpp>
+#include <spdlog/spdlog.h>
+#include <fmt/core.h>
+#include <fmt/format.h>
+
+namespace sam {
+
+
+}
+
+#endif //SAMPP_LUAUTILS_H
