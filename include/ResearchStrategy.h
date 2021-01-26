0e108d60ef91bf0f6b816ef70ba960bfb7da8f63
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-01-26 11:21:37 +0100

    clang-formatted

--- include/ResearchStrategy.h
+++ include/ResearchStrategy.h
@@ -19,208 +19,224 @@
 #ifndef SAMPP_RESEARCHSTRATEGY_H
 #define SAMPP_RESEARCHSTRATEGY_H
 
-#include <spdlog/spdlog.h>
 #include <fmt/core.h>
 #include <fmt/format.h>
+#include <spdlog/spdlog.h>
+
 #include <sol/sol.hpp>
 
-#include "Experiment.h"
 #include "Distributions.h"
+#include "Experiment.h"
 #include "Policy.h"
 
 namespace sam {
 
-enum class DecisionMethod {
-  DefaultResearchStrategy
-};
+enum class DecisionMethod { DefaultResearchStrategy };
 
-NLOHMANN_JSON_SERIALIZE_ENUM(
-    DecisionMethod,
-    {{DecisionMethod::DefaultResearchStrategy, "DefaultResearchStrategy"}})
+NLOHMANN_JSON_SERIALIZE_ENUM(DecisionMethod,
+                             {{DecisionMethod::DefaultResearchStrategy,
+                               "DefaultResearchStrategy"}})
 
 using SubmissionPool = std::vector<Submission>;
-
 
 ///
 /// @brief      Abstract class for different decision strategies.
 ///
 class ResearchStrategy {
-
-public:
+ public:
   // Lua state
   sol::state lua;
-  
+
   virtual ~ResearchStrategy() = 0;
   ResearchStrategy();
-  
+
   /// ResearchStrategy factory method.
   ///
   /// @param research_strategy_config    A JSON object containing information
   /// about each research strategy.
-  static std::unique_ptr<ResearchStrategy>
-  build(json &research_strategy_config);
-  
+  static std::unique_ptr<ResearchStrategy> build(
+      json &research_strategy_config);
+
   std::optional<SubmissionPool> submission_candidates;
-  
+
   //! List of selected submissions collected by the researcher
   //! using the stashing_policy
   SubmissionPool stashed_submissions;
-  
+
   //! List of submissions selecting during the hacking procedure
   SubmissionPool hacked_submissions;
 
-  /// @todo These guys should move to their own class, I don't have to keep everything here!
+  /// @todo These guys should move to their own class, I don't have to keep
+  /// everything here!
   PolicyChainSet initial_selection_policies;
   PolicyChain submission_decision_policies;
   PolicyChainSet between_stashed_selection_policies;
   PolicyChainSet between_reps_policies;
-  
+
   PolicyChain will_start_hacking_decision_policies;
 
   PolicyChain will_continue_replicating_decision_policy;
-  
+
   PolicyChain stashing_policy;
-  
-  
-  /// @brief  Indicates whether the researcher will start going to the hacking procedure.
-  /// The default here is to not go for hacking if we already have one candidate; but this
-  /// can be overridden in different decision strategies
+
+  /// @brief  Indicates whether the researcher will start going to the hacking
+  /// procedure. The default here is to not go for hacking if we already have
+  /// one candidate; but this can be overridden in different decision strategies
   virtual bool willStartHacking() { return false; };
-  
-  /// @brief  This will be used by `hackTheResearch` and uses the decision policy to decide
-  /// whether the next hacking strategy is going to be executed !
+
+  /// @brief  This will be used by `hackTheResearch` and uses the decision
+  /// policy to decide whether the next hacking strategy is going to be executed
+  /// !
   ///
   /// @param  experiment A reference to the experiment
   virtual bool willContinueHacking(Experiment *experiment,
-                                   PolicyChain &pchain) {return false; };
-  
-  
+                                   PolicyChain &pchain) {
+    return false;
+  };
+
   /// Submission
-  bool willBeSubmitting(const std::optional<SubmissionPool>& sub,
+  bool willBeSubmitting(const std::optional<SubmissionPool> &sub,
                         PolicyChain &pchain);
-  
-  virtual bool willContinueReplicating(PolicyChain &pchain) {return false; };
-  
-  /// @todo This needs to be private but currently, I don't have a good place to put it.
-  /// The verdict system is broken, and if reset it after the selectionBetweenSubmission, it's weird
-  /// and I cannot just call it in any other methods because then it's hidden
+
+  virtual bool willContinueReplicating(PolicyChain &pchain) { return false; };
+
+  /// @todo This needs to be private but currently, I don't have a good place to
+  /// put it. The verdict system is broken, and if reset it after the
+  /// selectionBetweenSubmission, it's weird and I cannot just call it in any
+  /// other methods because then it's hidden
   ///
   /// Reset the internal state of the research strategy
   void reset() {
     hacked_submissions.clear();
     stashed_submissions.clear();
     submission_candidates.reset();
-//    clear();
+    //    clear();
   }
-  
+
   /// @brief      Implementation of decision-making procedure.
-  virtual ResearchStrategy &selectOutcomeFromExperiment(Experiment *experiment,
-                                    PolicyChainSet &pchain_set) = 0;
-  
-  virtual ResearchStrategy &selectOutcomeFromPool(SubmissionPool &spool,
-                                    PolicyChainSet &pchain_set) = 0;
+  virtual ResearchStrategy &selectOutcomeFromExperiment(
+      Experiment *experiment, PolicyChainSet &pchain_set) = 0;
+
+  virtual ResearchStrategy &selectOutcomeFromPool(
+      SubmissionPool &spool, PolicyChainSet &pchain_set) = 0;
 
   /// Create and save all possible submissions from an experiment, if they pass
   /// the given policy predicate
   ///
   /// @param experiment a reference to the experiment
-  /// @param pchain a policy chain, usually stored in `stashing_policy` in the config file
+  /// @param pchain a policy chain, usually stored in `stashing_policy` in the
+  /// config file
   void saveOutcomes(Experiment &experiment, PolicyChain &pchain);
-  
-protected:
-  
+
+ protected:
   void selectOutcome(Experiment &experiment, PolicyChainSet &pchain_set);
 
-  void selectBetweenSubmissions(SubmissionPool &spool, PolicyChainSet &pchain_set);
-
-  
+  void selectBetweenSubmissions(SubmissionPool &spool,
+                                PolicyChainSet &pchain_set);
 };
 
 ///
 /// @ingroup    ResearchStrategies
 class DefaultResearchStrategy final : public ResearchStrategy {
-
-public:
+ public:
   struct Parameters {
     DecisionMethod name = DecisionMethod::DefaultResearchStrategy;
 
     std::vector<std::vector<std::string>> initial_selection_policies_defs;
     std::vector<std::string> submission_decision_policies_defs;
-    std::vector<std::vector<std::string>> between_stashed_selection_policies_defs;
-    std::vector<std::vector<std::string>> between_replications_selection_policies_defs;
-    
+    std::vector<std::vector<std::string>>
+        between_stashed_selection_policies_defs;
+    std::vector<std::vector<std::string>>
+        between_replications_selection_policies_defs;
+
     std::vector<std::string> will_start_hacking_decision_policies_def;
-    
+
     std::vector<std::string> will_continue_replicating_decision_policy_def;
-    
+
     std::vector<std::string> stashing_policy_def;
   };
 
   Parameters params;
 
   explicit DefaultResearchStrategy(const Parameters &p) : params{p} {
-
     spdlog::trace("Preparing Initial Selection Policies: ");
-    initial_selection_policies = PolicyChainSet(p.initial_selection_policies_defs, lua);
+    initial_selection_policies =
+        PolicyChainSet(p.initial_selection_policies_defs, lua);
 
     spdlog::trace("Preparing Between Stashed Selection Policies: ");
-    between_stashed_selection_policies = PolicyChainSet(p.between_stashed_selection_policies_defs, lua);
+    between_stashed_selection_policies =
+        PolicyChainSet(p.between_stashed_selection_policies_defs, lua);
 
     spdlog::trace("Preparing Submission Decision Policies: ");
-    submission_decision_policies = PolicyChain(p.submission_decision_policies_defs, PolicyChainType::Decision, lua);
-    
+    submission_decision_policies = PolicyChain(
+        p.submission_decision_policies_defs, PolicyChainType::Decision, lua);
+
     spdlog::trace("Preparing Between Replication Selection Policies: ");
-    between_reps_policies = PolicyChainSet(p.between_replications_selection_policies_defs, lua);
-    
+    between_reps_policies =
+        PolicyChainSet(p.between_replications_selection_policies_defs, lua);
+
     spdlog::trace("Preparing Will Start Hacking Decision Policies: ");
-    will_start_hacking_decision_policies = PolicyChain(p.will_start_hacking_decision_policies_def, PolicyChainType::Decision, lua);
-    
+    will_start_hacking_decision_policies =
+        PolicyChain(p.will_start_hacking_decision_policies_def,
+                    PolicyChainType::Decision, lua);
+
     spdlog::trace("Preparing Will Conitnue Replicating Decision Policies: ");
-    will_continue_replicating_decision_policy = PolicyChain(p.will_continue_replicating_decision_policy_def, PolicyChainType::Decision, lua);
-    
+    will_continue_replicating_decision_policy =
+        PolicyChain(p.will_continue_replicating_decision_policy_def,
+                    PolicyChainType::Decision, lua);
+
     spdlog::trace("Preparing Stashing Policies: ");
-    stashing_policy = PolicyChain(p.stashing_policy_def, PolicyChainType::Selection, lua);
+    stashing_policy =
+        PolicyChain(p.stashing_policy_def, PolicyChainType::Selection, lua);
   };
 
-  ResearchStrategy &selectOutcomeFromExperiment(Experiment *experiment,
-                                    PolicyChainSet &pchain_set) override;
-  
+  ResearchStrategy &selectOutcomeFromExperiment(
+      Experiment *experiment, PolicyChainSet &pchain_set) override;
+
   ResearchStrategy &selectOutcomeFromPool(SubmissionPool &spool,
-                                    PolicyChainSet &pchain_set) override;
+                                          PolicyChainSet &pchain_set) override;
 
   bool willStartHacking() override;
-  
+
   bool willContinueHacking(Experiment *experiment,
-                                   PolicyChain &pchain) override;
-  
+                           PolicyChain &pchain) override;
+
   bool willContinueReplicating(PolicyChain &pchain) override;
 };
 
 // JSON Parser for DefaultResearchStrategy::Parameters
 inline void to_json(json &j, const DefaultResearchStrategy::Parameters &p) {
-  j = json{{"name", p.name},
-           {"initial_selection_policies", p.initial_selection_policies_defs},
-           {"submission_decision_policies", p.submission_decision_policies_defs},
-           {"between_stashed_selection_policies", p.between_stashed_selection_policies_defs},
-           {"between_replications_selection_policies", p.between_replications_selection_policies_defs},
-          {"will_start_hacking_decision_policies", p.will_start_hacking_decision_policies_def},
-          {"will_continue_replicating_decision_policy", p.will_continue_replicating_decision_policy_def},
-          {"stashing_policy", p.stashing_policy_def}
-  };
+  j = json{
+      {"name", p.name},
+      {"initial_selection_policies", p.initial_selection_policies_defs},
+      {"submission_decision_policies", p.submission_decision_policies_defs},
+      {"between_stashed_selection_policies",
+       p.between_stashed_selection_policies_defs},
+      {"between_replications_selection_policies",
+       p.between_replications_selection_policies_defs},
+      {"will_start_hacking_decision_policies",
+       p.will_start_hacking_decision_policies_def},
+      {"will_continue_replicating_decision_policy",
+       p.will_continue_replicating_decision_policy_def},
+      {"stashing_policy", p.stashing_policy_def}};
 }
 
 inline void from_json(const json &j, DefaultResearchStrategy::Parameters &p) {
   j.at("name").get_to(p.name);
   j.at("initial_selection_policies").get_to(p.initial_selection_policies_defs);
-  j.at("submission_decision_policies").get_to(p.submission_decision_policies_defs);
-  j.at("between_stashed_selection_policies").get_to(p.between_stashed_selection_policies_defs);
-  j.at("between_replications_selection_policies").get_to(p.between_replications_selection_policies_defs);
-  j.at("will_start_hacking_decision_policies").get_to(p.will_start_hacking_decision_policies_def);
-  j.at("will_continue_replicating_decision_policy").get_to(p.will_continue_replicating_decision_policy_def);
+  j.at("submission_decision_policies")
+      .get_to(p.submission_decision_policies_defs);
+  j.at("between_stashed_selection_policies")
+      .get_to(p.between_stashed_selection_policies_defs);
+  j.at("between_replications_selection_policies")
+      .get_to(p.between_replications_selection_policies_defs);
+  j.at("will_start_hacking_decision_policies")
+      .get_to(p.will_start_hacking_decision_policies_def);
+  j.at("will_continue_replicating_decision_policy")
+      .get_to(p.will_continue_replicating_decision_policy_def);
   j.at("stashing_policy").get_to(p.stashing_policy_def);
 }
 
+}  // namespace sam
 
-} // namespace sam
-
-#endif // SAMPP_RESEARCHSTRATEGY_H
+#endif  // SAMPP_RESEARCHSTRATEGY_H
--- src/ResearchStrategy.cpp
+++ src/ResearchStrategy.cpp
@@ -20,7 +20,7 @@ using namespace sam;
 /// Pure destructors of ResearchStrategy. This is necessary for proper
 /// deconstruction of derived classes.
 ///
-ResearchStrategy::~ResearchStrategy(){
+ResearchStrategy::~ResearchStrategy() {
   // pure destructors
 }
 
@@ -30,40 +30,34 @@ ResearchStrategy::~ResearchStrategy(){
 /// DefaultResearchStrategy
 ///
 ResearchStrategy::ResearchStrategy() {
-
   // Preparing a Lua instance, and registering my types there
   lua.open_libraries();
 
   // Registering the DependentVariable
-  lua.new_usertype<DependentVariable>("DependentVariable",
-   "id", &DependentVariable::id_,
-   "nobs", &DependentVariable::nobs_,
-   "mean", &DependentVariable::mean_,
-   "pvalue", &DependentVariable::pvalue_,
-   "effect", &DependentVariable::effect_,
-   "sig", &DependentVariable::sig_,
-   "hacked", &DependentVariable::is_hacked_,
-   "candidate", &DependentVariable::is_candidate_
-   );
+  lua.new_usertype<DependentVariable>(
+      "DependentVariable", "id", &DependentVariable::id_, "nobs",
+      &DependentVariable::nobs_, "mean", &DependentVariable::mean_, "pvalue",
+      &DependentVariable::pvalue_, "effect", &DependentVariable::effect_, "sig",
+      &DependentVariable::sig_, "hacked", &DependentVariable::is_hacked_,
+      "candidate", &DependentVariable::is_candidate_);
 
   // Registering the Submission
-  lua.new_usertype<Submission>("Submission",
-      "id", sol::property([](Submission &s) { return s.dv_.id_; }),
-      "nobs", sol::property([](Submission &s) { return s.dv_.nobs_; }),
-      "mean", sol::property([](Submission &s) { return s.dv_.mean_; }),
-      "pvalue", sol::property([](Submission &s) { return s.dv_.pvalue_; }),
-      "effect", sol::property([](Submission &s) { return s.dv_.effect_; }),
-      "sig", sol::property([](Submission &s) { return s.dv_.sig_; }),
-      "hacked", sol::property([](Submission &s) { return s.dv_.is_hacked_; }),
-      "candidate", sol::property([](Submission &s) { return s.dv_.is_candidate_; }));
+  lua.new_usertype<Submission>(
+      "Submission", "id",
+      sol::property([](Submission &s) { return s.dv_.id_; }), "nobs",
+      sol::property([](Submission &s) { return s.dv_.nobs_; }), "mean",
+      sol::property([](Submission &s) { return s.dv_.mean_; }), "pvalue",
+      sol::property([](Submission &s) { return s.dv_.pvalue_; }), "effect",
+      sol::property([](Submission &s) { return s.dv_.effect_; }), "sig",
+      sol::property([](Submission &s) { return s.dv_.sig_; }), "hacked",
+      sol::property([](Submission &s) { return s.dv_.is_hacked_; }),
+      "candidate",
+      sol::property([](Submission &s) { return s.dv_.is_candidate_; }));
 }
 
-
-std::unique_ptr<ResearchStrategy>
-ResearchStrategy::build(json &research_strategy_config) {
-
+std::unique_ptr<ResearchStrategy> ResearchStrategy::build(
+    json &research_strategy_config) {
   if (research_strategy_config["name"] == "DefaultResearchStrategy") {
-
     auto params =
         research_strategy_config.get<DefaultResearchStrategy::Parameters>();
     return std::make_unique<DefaultResearchStrategy>(params);
@@ -73,67 +67,58 @@ ResearchStrategy::build(json &research_strategy_config) {
   }
 }
 
-
 ///
 /// Select an unique outcome from an experiment, if at some point, a PolicyChain
 /// finds a group of outcomes instead of a unique outcome, the selection will be
 /// saved and the will await for processing in a different stages.
 ///
-/// We check all available PolicyChains in the given chain set, and will stop as soon
-/// as any chain returns __something__!
+/// We check all available PolicyChains in the given chain set, and will stop as
+/// soon as any chain returns __something__!
 ///
 /// @param experiment a reference to an experiment
 /// @param pchain_set a reference to a policy chain set
 ///
 void ResearchStrategy::selectOutcome(Experiment &experiment,
                                      PolicyChainSet &pchain_set) {
-  
   for (auto &pchain : pchain_set) {
-    
     submission_candidates = pchain(experiment);
-    
+
     /// If any of the pchains return something, we ignore the rest, and leave!
     if (submission_candidates) {
       return;
     }
-      
   }
-
 }
 
-
-/// Select a unique submission from the given pool of submissions. If none of the submissions
-/// satisfies all the policies, we just return, and submission_candidate will not be rewritten.
+/// Select a unique submission from the given pool of submissions. If none of
+/// the submissions satisfies all the policies, we just return, and
+/// submission_candidate will not be rewritten.
 ///
-/// @note If submission_candidate is empty to this point, and we cannot find anything here,
-/// we'll continue with nothing, and the current experiment will most likely be discarded.
+/// @note If submission_candidate is empty to this point, and we cannot find
+/// anything here, we'll continue with nothing, and the current experiment will
+/// most likely be discarded.
 ///
-/// @param spool a collection of submissions collected in previous stages, e.g., selectOutcome
+/// @param spool a collection of submissions collected in previous stages, e.g.,
+/// selectOutcome
 /// @param pchain_set a set of policy chains
 void ResearchStrategy::selectBetweenSubmissions(SubmissionPool &spool,
                                                 PolicyChainSet &pchain_set) {
-  
-  
   /// If there is no policy defined, then, we don't do any filtering and returns
   /// all of the candidates
   if (pchain_set.empty()) {
     submission_candidates = spool;
-    return;   // we just don't have anything to work with
+    return;  // we just don't have anything to work with
   }
 
   for (auto &pchain : pchain_set) {
-    
     submission_candidates = pchain(spool);
-    
+
     /// If any of the pchains return something, we ignore the rest, and leave!
     if (submission_candidates) {
       return;
     }
-
   }
-
 }
-
 
 /// Create and save all possible submissions from an experiment, if
 /// the satisfy all of the given policies in the pchain.
@@ -141,37 +126,35 @@ void ResearchStrategy::selectBetweenSubmissions(SubmissionPool &spool,
 /// @param      experiment  a reference to the experiment
 /// @param      pchain      a policy chain, usually stored in
 ///                         `stashing_policy` in the config file
- void ResearchStrategy::saveOutcomes(Experiment &experiment, PolicyChain &pchain) {
-    
-   if (not pchain.empty()) {
-     
+void ResearchStrategy::saveOutcomes(Experiment &experiment,
+                                    PolicyChain &pchain) {
+  if (not pchain.empty()) {
     spdlog::debug("Stashing...");
-   
+
     auto selections = pchain(experiment);
-    
+
     if (selections) {
-      stashed_submissions.insert(stashed_submissions.end(), selections.value().begin(),
-                              selections.value().end());
+      stashed_submissions.insert(stashed_submissions.end(),
+                                 selections.value().begin(),
+                                 selections.value().end());
     }
-    
-   }
-   
+  }
 }
 
 ///
 /// @brief      Indicates whether the Researcher is going to submit the given
 /// set of submissions or not.
-/// 
+///
 /// Usually `submission_decision_policies` will be used to evaluate the quality
 /// of the submission pool
 ///
 /// @param[in]  subs    A list of Submission(s)
-/// @param      pchain  A policy chain 
+/// @param      pchain  A policy chain
 ///
 /// @return     returns `true` if submission will undergo
 ///
-bool ResearchStrategy::willBeSubmitting(const std::optional<std::vector<Submission>>& subs, PolicyChain &pchain) {
-  
+bool ResearchStrategy::willBeSubmitting(
+    const std::optional<std::vector<Submission>> &subs, PolicyChain &pchain) {
   if (pchain.empty())
     return true;
   else {
@@ -179,17 +162,14 @@ bool ResearchStrategy::willBeSubmitting(const std::optional<std::vector<Submissi
     if (subs) {
       bool check{false};
       for (const auto &sub : subs.value()) {
-        check |=
-            std::all_of(pchain.begin(), pchain.end(),
-                        [&](auto &policy) -> bool { return policy(sub); });
+        check |= std::all_of(pchain.begin(), pchain.end(),
+                             [&](auto &policy) -> bool { return policy(sub); });
       }
       return check;
     } else
       return false;
   }
-  
 }
-
 
 /// @brief  Decides whether Researcher is going to hacking the Experiment
 ///
@@ -197,108 +177,92 @@ bool ResearchStrategy::willBeSubmitting(const std::optional<std::vector<Submissi
 /// `will_start_hacking_decision_policies` roles; if yes, we will start hacking
 /// if no, then we will not continue to the hacking procedure
 bool DefaultResearchStrategy::willStartHacking() {
-  
   spdlog::trace("Checking whether to start hacking or not...");
 
   /// Start hacking if there is no criteria is defined
-  if (will_start_hacking_decision_policies.empty())
-    return true;
-    
-  
+  if (will_start_hacking_decision_policies.empty()) return true;
+
   if (submission_candidates) {
     spdlog::trace("Looking for: {}", will_start_hacking_decision_policies);
     spdlog::trace("Submission Candidates: {}", submission_candidates.value());
     /// @todo this can be replaced by Policy->operator()
-    
+
     /// Basically any of the candidates is good enough, then we're going
     /// to STOP hacking
-    bool verdict {false};
+    bool verdict{false};
     for (auto &sub : submission_candidates.value())
       verdict |= std::any_of(will_start_hacking_decision_policies.begin(),
                              will_start_hacking_decision_policies.end(),
-                             [&](auto &policy) -> bool {
-                                return policy(sub);
-                              });
+                             [&](auto &policy) -> bool { return policy(sub); });
     return verdict;
-    
-    
-//    return std::any_of(will_start_hacking_decision_policies.begin(), will_start_hacking_decision_policies.end(), [this](auto &policy) -> bool {
-//        return policy(this->submission_candidate.value());
-//        });
+
+    //    return std::any_of(will_start_hacking_decision_policies.begin(),
+    //    will_start_hacking_decision_policies.end(), [this](auto &policy) ->
+    //    bool {
+    //        return policy(this->submission_candidate.value());
+    //        });
   } else {
     spdlog::trace("No Candidate is available → Will Start Hacking");
     return true;
   }
-  
 }
 
-
-/// Determines whether the `final_submission_candidates` complies with **any** of the
-/// given policies.
+/// Determines whether the `final_submission_candidates` complies with **any**
+/// of the given policies.
 ///
-/// @note The important difference between this and `willBeSubmitting` is the fact that, the latter
-/// will check if **all** of the rules are passing.
+/// @note The important difference between this and `willBeSubmitting` is the
+/// fact that, the latter will check if **all** of the rules are passing.
 ///
 /// @todo This probably needs to be replaced by something inside the PolicyChain
 ///
 /// @param pchain a reference to the given policy chain
 bool DefaultResearchStrategy::willContinueHacking(Experiment *experiment,
-                                               PolicyChain &pchain) {
-  
+                                                  PolicyChain &pchain) {
   // Checking whether all policies are returning `true`
-  
-  if (pchain.empty())
-    return true;
-  
-  bool verdict {true};
-  for (int i{experiment->setup.nd()}, d{0}; i < experiment->setup.ng();
-  ++i, ++d %= experiment->setup.nd()) {
-    verdict &= std::any_of(pchain.begin(), pchain.end(), [&](auto &policy) -> bool {
-                  return policy(Submission{*experiment, i});
-                });
-  }
-  
-  return verdict;
-  
-}
 
+  if (pchain.empty()) return true;
+
+  bool verdict{true};
+  for (int i{experiment->setup.nd()}, d{0}; i < experiment->setup.ng();
+       ++i, ++d %= experiment->setup.nd()) {
+    verdict &=
+        std::any_of(pchain.begin(), pchain.end(), [&](auto &policy) -> bool {
+          return policy(Submission{*experiment, i});
+        });
+  }
+
+  return verdict;
+}
 
 bool DefaultResearchStrategy::willContinueReplicating(PolicyChain &pchain) {
-  
   // Checking whether all policies are returning `true`
-  
-  if (pchain.empty())
-    return true;
-  
+
+  if (pchain.empty()) return true;
+
   if (submission_candidates) {
-    
-    bool verdict {false};
+    bool verdict{false};
     for (auto &sub : submission_candidates.value())
-      verdict |= std::any_of(pchain.begin(),
-                             pchain.end(),
-                             [&](auto &policy) -> bool {
-                                return policy(sub);
-                              });
+      verdict |= std::any_of(pchain.begin(), pchain.end(),
+                             [&](auto &policy) -> bool { return policy(sub); });
     return not verdict;
-  }else{
+  } else {
     return true;
   }
-  
 }
 
-ResearchStrategy &DefaultResearchStrategy::selectOutcomeFromExperiment(Experiment *experiment,
-                                                PolicyChainSet &pchain_set) {
+ResearchStrategy &DefaultResearchStrategy::selectOutcomeFromExperiment(
+    Experiment *experiment, PolicyChainSet &pchain_set) {
   selectOutcome(*experiment, pchain_set);
-  
+
   /// @todo this is confusing and I need to change it
   saveOutcomes(*experiment, stashing_policy);
 
   return *this;
 }
 
-ResearchStrategy &DefaultResearchStrategy::selectOutcomeFromPool(SubmissionPool &spool,
-                                                PolicyChainSet &pchain_set) {
+ResearchStrategy &DefaultResearchStrategy::selectOutcomeFromPool(
+    SubmissionPool &spool, PolicyChainSet &pchain_set) {
   selectBetweenSubmissions(spool, pchain_set);
-  
+
   return *this;
 }
